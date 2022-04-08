e8b2629e3905e1d2526a3863e85882ea3a2e4a43
Raimondas Sileikis <raimondas.sileikis@gmail.com>
2022-04-08 11:10:40 +0300

    key

--- src/Components/Joke.jsx
+++ src/Components/Joke.jsx
@@ -2,6 +2,6 @@ export default function Joke({setup, delivery }) {
 
     return(
 
-        <li><i style={{color:'green'}} >{setup}</i>: <i style={{color:'blue'}} >{delivery}</i></li> 
+        <li><i style={{color:'green'}} >{setup}</i> <i style={{color:'blue'}} >{delivery}</i></li> 
     )
 }
